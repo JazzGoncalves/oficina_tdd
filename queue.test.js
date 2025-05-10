ecb2780843e061c22b8fad64cbcab88d5f392c42
JazzGoncalves <jessicagoncalves773@gmail.com>
2025-05-10 17:13:38 -0300

    exercicios de teste

--- queue.test.js
+++ queue.test.js
@@ -8,18 +8,31 @@ describe('Queue', () => {
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add('item1')
+    queue.add('item2')
+    expect(queue.size()).toBe(2)
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue()
+    queue.add('item')
+    expect(queue.elements).toEqual(['item'])
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add('primeiro')
+    queue.add('segundo')
+    expect(queue.peek()).toBe('primeiro')
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add('um')
+    queue.add('dois')
+    const removido = queue.dequeue()
+    expect(removido).toBe('um')
+    expect(queue.elements).toEqual(['dois'])
   })
 })
--- soma.js
+++ soma.js
@@ -1,5 +1,4 @@
-function soma() {
-  //seu código aqui
+function soma(a,b) {
+  return a + b
 }
-
 module.exports = soma
